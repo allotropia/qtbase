4b7fb0bab5e6292717148155fc9062493e697d01
Lorn Potter <lorn.potter@gmail.com>
2021-05-20 15:57:30 +1000

    wasm: fix tooltips show/hide behavior
    When tool tip was trying to show its window became activated causing
    tool tip to hide instantly due to a redundant window activation bug on
    show/hide/raise/lower.
    
    Fixes: QTBUG-91095
    Change-Id: Id215400a79cdc4ed1961731949ca139b575eec88
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>
    (cherry picked from commit a89754a2f322576d6d780976888ca517f48c26c3)

--- src/plugins/platforms/wasm/qwasmcompositor.cpp
+++ src/plugins/platforms/wasm/qwasmcompositor.cpp
@@ -111,6 +111,9 @@ void QWasmCompositor::addWindow(QWasmWindow *window, QWasmWindow *parentWindow)
     else
         m_compositedWindows[parentWindow].childWindows.append(window);
 
+    if (!QGuiApplication::focusWindow()) {
+        window->requestActivateWindow();
+    }
     notifyTopWindowChanged(window);
 }
 
@@ -126,6 +129,10 @@ void QWasmCompositor::removeWindow(QWasmWindow *window)
     m_windowStack.removeAll(window);
     m_compositedWindows.remove(window);
 
+    if (!m_windowStack.isEmpty() && !QGuiApplication::focusWindow()) {
+        auto lastWindow = m_windowStack.last();
+        lastWindow->requestActivateWindow();
+    }
     notifyTopWindowChanged(window);
 }
 
@@ -735,12 +742,13 @@ void QWasmCompositor::notifyTopWindowChanged(QWasmWindow *window)
     bool blocked = QGuiApplicationPrivate::instance()->isWindowBlocked(window->window(), &modalWindow);
 
     if (blocked) {
+        modalWindow->requestActivate();
         raise(static_cast<QWasmWindow*>(modalWindow->handle()));
         return;
     }
 
     requestRedraw();
-    QWindowSystemInterface::handleWindowActivated(window->window());
+
 }
 
 QWasmScreen *QWasmCompositor::screen()
