3816c43ae9671ffb72ff0ad28b5f9cd0b1344bd4
Jan-Marek Glogowski <glogow@fbihome.de>
2021-11-21 15:07:25 +0100

    Revert "wasm: fix window activation"
    This reverts commit 7482fd1e6828c5a4799e0e69a18622f04b5b9efe.

--- src/plugins/platforms/wasm/qwasmwindow.h
+++ src/plugins/platforms/wasm/qwasmwindow.h
@@ -74,7 +74,6 @@ public:
     QRect normalGeometry() const override;
     qreal devicePixelRatio() const override;
     void requestUpdate() override;
-    void requestActivateWindow() override;
 
     QWasmScreen *platformScreen() const;
     void setBackingStore(QWasmBackingStore *store) { m_backingStore = store; }
